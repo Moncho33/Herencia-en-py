6167d0374419cbe9567dd69a7fe856dcd4effafc
moncho <moncho.ct2023@gmail.com>
2024-04-19 13:31:49 -0500

    agregando clases para herencia

--- mainEquipo.py
+++ mainEquipo.py
@@ -1,5 +1,11 @@
-
+from EquipoTec import Equipot
+from Impresora import impresora
 
 if __name__=='__main__':
-   pass  
+    asus = Equipot(0, '', 0, '')
+    asus.ElegirEquipo()
+
+    hp = impresora(0, '', 0, '', '', '', 0)
+    hp.elegirImpresora()
+
 
